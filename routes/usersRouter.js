2ad2927a1993f1fead1c74a26ad72a8471286a88
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-02-10 17:33:36 +0500

    ewrqrewq
    ewrqewrq

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const { generateVerificationCode } = require('../middleware/file_upload');
 
 // Kullanıcı oluşturma
 router.post('/users', async (req, res) => {
